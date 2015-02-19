9c0e2d081ec8d2951febce58419cc9b338573ffd
Sanjay Saxena <sanjay.saxena@kaazing.com>
2015-02-19 13:45:49 -0800

    Moved ExtensionsIT into a separate package such that it is not with specificiation.ws ITs.

--- src/test/java/org/kaazing/netx/ws/ext/ExtensionsIT.java
+++ src/test/java/org/kaazing/netx/ws/ext/ExtensionsIT.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kaazing.netx.ws.specification;
+package org.kaazing.netx.ws.ext;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.Assert.assertEquals;
