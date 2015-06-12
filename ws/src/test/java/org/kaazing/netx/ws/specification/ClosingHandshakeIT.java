ac5f7f857866949acc67e427819a64c052ec7684
Sanjay Saxena <sanjay.saxena@kaazing.com>
2015-06-12 15:23:13 -0700

    Update to the latest k3po 2.0.1 version.

--- ws/src/test/java/org/kaazing/netx/ws/specification/ClosingHandshakeIT.java
+++ ws/src/test/java/org/kaazing/netx/ws/specification/ClosingHandshakeIT.java
@@ -28,7 +28,6 @@ import java.io.Reader;
 import java.net.URI;
 import java.util.Random;
 
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.DisableOnDebug;
@@ -228,7 +227,6 @@ public class ClosingHandshakeIT {
         k3po.finish();
     }
 
-    @Ignore
     @Test
     @Specification({
         "server.send.close.frame.with.code.1000.and.reason/handshake.response.and.frame" })
